86cb27ae4b95f02c7f53cc210f58432cd4a6ee84
marlyne <=>
2023-03-22 18:27:48 +0100

    DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
@@ -21,18 +22,3 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-
-else:
-    class Place(BaseModel):
-        """ A place to stay """
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
