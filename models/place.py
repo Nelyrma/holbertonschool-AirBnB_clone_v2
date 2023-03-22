eb24fc9389d3e83bb7b7a75624449706796770e9
marlyne <=>
2023-03-22 19:47:19 +0100

    DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -7,18 +7,17 @@ from sqlalchemy.orm import relationship, backref
 from os import getenv
 
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class Place(BaseModel, Base):
-        """ A place to stay """
-        __tablename__ = 'places'
+class Place(BaseModel, Base):
+    """ A place to stay """
+     __tablename__ = 'places'
 
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, default=0, nullable=False)
-        number_bathrooms = Column(Integer, default=0, nullable=False)
-        max_guest = Column(Integer, default=0, nullable=False)
-        price_by_night = Column(Integer, default=0, nullable=False)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
