e74e67f1250f19a2c044ac714e8f2800d977d488
scorpion00100 <danbetheliryivuze@gmail.com>
2023-03-23 12:41:35 +0100

    adding relashionship reviesw

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import DateTime
 from sqlalchemy.sql.schema import Column, ForeignKey
@@ -12,7 +12,7 @@ from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     class User(BaseModel, Base):
-        """This class defines a user by various attributes"""
+        """class defines a user by various attributes"""
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
@@ -20,10 +20,12 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         last_name = Column(String(128))
         places = relationship("Place", backref="user",
                               cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
 
 else:
     class User(BaseModel):
-        """This class defines a user by various attributes"""
+        """class defines a user by various attributes"""
         email = ''
         password = ''
         first_name = ''
