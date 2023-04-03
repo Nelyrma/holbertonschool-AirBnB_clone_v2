3d9f23e28de2f7bee71b450da19a18344c851f49
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-03 11:25:02 +0200

    script that starts a Flask web application with another route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Flask web aplication '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def view():
+    ''' return this message to the user '''
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def view_hbnb():
+    ''' return this 'hbnb' to the user '''
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
